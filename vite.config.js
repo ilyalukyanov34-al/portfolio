ba7386d3bb9e89c74e02a80cefa9e5dd2887a68c
- <ilyalukyanov34>
2026-06-10 10:49:34 +0500

    build: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/react-refresh"; // или @vitejs/plugin-react в зависимости от твоей версии
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/portfolio/", // 👈 ОБЯЗАТЕЛЬНО ДОБАВЬ ЭТУ СТРОКУ (слэши с двух сторон важны!)
+});
