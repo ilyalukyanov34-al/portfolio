a28aa226885a18cf9416ce34fb607fdda40c1737
- <ilyalukyanov34>
2026-06-17 17:12:15 +0500

    error width language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,6 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
-import trasnlationEn from "../locales/en/translation.json";
+import translationEn from "../locales/en/translation.json";
 import translationRU from "../locales/ru/translation.json";
 
 const resources = {
@@ -10,21 +8,21 @@ const resources = {
     translation: translationRU,
   },
   en: {
-    translation: trasnlationEn,
+    translation: translationEn,
   },
 };
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
+    lng: "ru",
     fallbackLng: "ru",
-    debug: true,
+    debug: false,
     interpolation: {
       escapeValue: false,
     },
   });
 
 export default i18n;
+
